fb47e028eec1af97cafa04fee2d21e7b0a2b158f
pdeleon-source <pauldeleon917@gmail.com>
2024-02-28 15:43:45 -0500

    Began to piece file

---
+++ pieces.py
@@ -0,0 +1,19 @@
+# for rooks in each turn, one value in the coordinate cannot change
+# for bishops, both values must change
+
+
+class Pawn():
+
+
+class Rook(self, ):
+    def move(self, new_position):
+        current_row, current_col = self.position
+        new_row, new_col = new_position
+
+class Knight():
+
+class Bishop():
+
+class Queen():
+
+class King():
