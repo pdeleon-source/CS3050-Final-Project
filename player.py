d0a04d8a01d4f0dd667e9ccb5de428c437b830a6
Alex Stute <stute5000@gmail.com>
2024-02-28 16:19:19 -0500

    started init for Player

---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self):
+        # save the pieces as a list of piece objects
+        # 8 pawns, 2 rooks, 2 knights, 2 bishops, 1 queen, 1 king
+        self.pieces = []
