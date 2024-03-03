d4115a6c0ddb5aa1bbac8a7d124c764cfdb665a9
hannah <hgsherid@uvm.edu>
2024-03-03 15:06:06 -0500

    Showed available moves for each piece

--- pieces.py
+++ pieces.py
@@ -4,14 +4,14 @@ import board
 
 
 class Bishop:
-    def __init__(self, allegiance, board, current_row, current_col):
+    def __init__(self, allegiance, board, current_pos):
         points = 3
         self.moves = 0
         self.allegiance = allegiance
-        self.current_row = current_row
-        self.current_col = current_col
+        self.current_row = current_pos[0]
+        self.current_col = current_pos[1]
         self.board = board
-        self.board[self.current_col][self.current_row] = self
+        self.board[self.current_row][self.current_col] = self
 
     def move(self, new_row, new_col) -> bool:
         """
@@ -47,19 +47,33 @@ class Bishop:
             print("Invalid move!")
             return False
 
+    def available_moves(self):
+        movements = []
+
+        # Bishop moves diagonally, so we check all four diagonal directions
+        for diagonal_row, diagonal_col in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            row, col = self.current_row + diagonal_row, self.current_col + diagonal_col
+            while 0 <= row < 8 and 0 <= col < 8:
+                movements.append((row, col))
+
+                row += diagonal_row
+                col += diagonal_col
+
+        return movements
+
     def __repr__(self):
         return f"{self.allegiance} Bishop"
 
 
 class Queen:
-    def __init__(self, allegiance, board, current_row, current_col):
+    def __init__(self, allegiance, board, current_pos):
         points = 4
         self.moves = 0
         self.allegiance = allegiance
-        self.current_row = current_row
-        self.current_col = current_col
+        self.current_row = current_pos[0]
+        self.current_col = current_pos[1]
         self.board = board
-        self.board[self.current_col][self.current_row] = self
+        self.board[self.current_row][self.current_col] = self
 
     def move(self, new_row, new_col):
         if new_row == self.current_row and new_col == self.current_col:
@@ -88,31 +102,45 @@ class Queen:
             print("Invalid move!")
             return False
 
+    def available_moves(self):
+        movements = []
+
+        # Queen moves diagonally, so we check all four diagonal directions
+        for diagonal_row, diagonal_col in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            row, col = self.current_row + diagonal_row, self.current_col + diagonal_col
+            while 0 <= row < 8 and 0 <= col < 8:
+                movements.append((row, col))
+
+                row += diagonal_row
+                col += diagonal_col
+
+        # Queen may also move vertically and horizontally
+        # TODO: Includes current position
+        for i in range(0, 8):
+            movements.append((self.current_row, i))
+            movements.append((i, self.current_col))
+
+        return movements
+
     def __repr__(self):
         return f"{self.allegiance} Queen"
 
 
 class King:
-    def __init__(self, allegiance, board, current_row, current_col):
+    def __init__(self, allegiance, board, current_pos):
         points = 4
         self.moves = 0
         self.allegiance = allegiance
         self.board = board
-        self.current_row = current_row
-        self.current_col = current_col
-        self.board[self.current_col][self.current_row] = self
+        self.current_row = current_pos[0]
+        self.current_col = current_pos[1]
+        self.board[self.current_row][self.current_col] = self
 
     def move(self, new_row, new_col):
         destination = self.board[new_row][new_col]
-        # Can only move one square
-        if new_row == self.current_row and abs(new_col - self.current_col) == 1:
-            if destination is not None and destination.allegiance != self.allegiance:
-                print(f"Captured {destination} at position ({new_row}, {new_col})")
-            elif destination is not None:
-                print("Cannot capture that piece!")
-                return False
-
-        elif new_col == self.current_col and abs(new_row - self.current_row) == 1:
+        if new_row == self.current_row and abs(new_col - self.current_col) == 1 or new_col == self.current_col and abs(
+                new_row - self.current_row) == 1 or abs(new_row - self.current_row) == 1 and abs(
+                new_col - self.current_col) == 1:
             if destination is not None and destination.allegiance != self.allegiance:
                 print(f"Captured {destination} at position ({new_row}, {new_col})")
             elif destination is not None:
@@ -132,6 +160,8 @@ class King:
         self.current_col = new_col
         return True
 
+    def available_moves(self):
+        movements = []
 
     def __repr__(self):
         return f"{self.allegiance} King"
@@ -140,15 +170,15 @@ class King:
 if __name__ == "__main__":
     chess_board = [[None for _ in range(8)] for _ in range(8)]
 
-   # bish = Bishop("Black", chess_board, 0, 0)
-    bish2 = Bishop("White", chess_board, 0, 0)
-    queen = Queen("Black", chess_board, 2, 2)
-    king = King("White", chess_board, 1, 1)
+    # bish = Bishop("Black", chess_board, 0, 0)
+    #bish = Bishop("White", chess_board, [3, 3])
+    queen = Queen("Black", chess_board, [5, 3])
+    #king = King("White", chess_board, 2, 2)
 
     for row in chess_board:
         print(row)
 
-    print(king.move(2, 2))
+    print(queen.available_moves())
 
     for row in chess_board:
         print(row)
