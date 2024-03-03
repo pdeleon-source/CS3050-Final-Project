5408ef0283e76d061ac0450ba8d0533a614ddd0f
Alex Stute <stute5000@gmail.com>
2024-03-03 13:25:59 -0500

    fixed another allegiance typo; MovePiece calls the pieces move method

--- player.py
+++ player.py
@@ -1,19 +1,19 @@
 class Player:
 
     # base constructor
-    # params: allegience (assigned by Board)
+    # params: allegiance (assigned by Board)
     #         pieces (given by Board)
     #         available moves (again, from Board)
-    def __init__(self, allegience, pieces, available_moves):
+    def __init__(self, allegiance, pieces, available_moves):
         self.pieces = pieces
-        self.allegience = allegience
+        self.allegiance = allegiance
         self.available_moves = available_moves
 
     # select a valid piece on the board
     # params: the piece to be selected
     def SelectPiece(self, piece):
         # if the piece and player allegience don't match, don't select
-        if piece.allegience != self.allegience:
+        if piece.allegiance != self.allegiance:
             return
         # otherwise, select
         # Not sure what to do for this right now, have this as filler
@@ -23,9 +23,8 @@ class Player:
 
     # move a selected piece to a specified square
     # params: the piece to be moved, the square to move to
-    def MovePiece(self, piece, square):
+    def MovePiece(self, piece, row, column):
         # TODO: Again, just some filler for the time to test a bit. Not final
-        piece.location = square
-        # TODO: After moving the piece, need to update the board appropriately
-        # Remove piece from old location, and add it to new location
+        piece.move(row, column)
+
         pass
