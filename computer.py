65b709bc142bf5d692f7f4db7f62a1bcf2bc659f
Alex Stute <stute5000@gmail.com>
2024-03-04 16:59:40 -0500

    added a couple comment

--- computer.py
+++ computer.py
@@ -1,3 +1,6 @@
+# this will be a class that can move pieces legally around the chess board
+# Note: Doesn't have to be smart, just has to be "correct"
 class Computer:
     def __init__(self):
         pass
+
