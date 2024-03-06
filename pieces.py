ae3a7a557e9c434d29e3a8900942c61e424be182
hannah <hgsherid@uvm.edu>
2024-03-06 13:32:38 -0500

    Created parent "Piece" class and modified moves functions

--- pieces.py
+++ pieces.py
@@ -3,10 +3,46 @@
 import board
 import arcade
 
-class Piece():
-    def something(self):
-        print("something")
 
+class Piece():
+    def __init__(self, allegiance, board, current_pos, texture):
+        self.moves = 0
+        self.allegiance = allegiance
+        self.board = board
+        self.current_row = current_pos[0]
+        self.current_col = current_pos[1]
+        self.board[self.current_row][self.current_col] = self
+
+        self.texture = arcade.load_texture(texture)
+
+    def move(self, new_pos) -> bool:
+        new_row = new_pos[0]
+        new_col = new_pos[1]
+
+        if (new_row, new_col) not in self.available_moves():
+            print("INVALID MOVE")
+            return False
+
+        destination = self.board[new_row][new_col]
+        if destination is not None and destination.allegiance != self.allegiance:
+            print(f"Captured {destination} at position ({new_row}, {new_col})")
+        elif destination is not None:
+            print(f"Cannot capture {destination}!")
+            return False
+
+        # All conditions passed so move Bishop piece
+        print(f"Moved {self} to position ({new_row}, {new_col})")
+
+        self.board[self.current_row][self.current_col] = None
+        self.board[new_row][new_col] = self
+
+        # Update variables
+        self.moves += 1
+        self.current_row = new_row
+        self.current_col = new_col
+        return True
+
+"""
 class Pawn(Piece):
     def __init__(self, allegiance, board, current_row, current_col):
         points = 1
@@ -42,7 +78,7 @@ class Pawn(Piece):
     def __repr__(self):
         return f"{self.allegiance} Pawn"
 
-class Rook:
+class Rook(Piece):
     def __init__(self, allegiance, board, current_row, current_col):
         points = 5
         self.moves = 0
@@ -68,48 +104,16 @@ class Rook:
         # vertical movement
         elif new_row == self.current_row and new_col != self.current_col:
             destination = self.board[new_row][new_col]
+"""
+
+
 class Bishop(Piece):
-    def __init__(self, allegiance, board, current_pos):
-        points = 3
-        self.moves = 0
-        self.allegiance = allegiance
-        self.current_row = current_pos[0]
-        self.current_col = current_pos[1]
-        self.board = board
-        #self.board[self.current_col][self.current_row] = self
-        if allegiance == 'Black':
-            self.texture = arcade.load_texture('pieces_png/black-bishop.png')
-        else:
-            self.texture = arcade.load_texture('pieces_png/white-bishop.png')
-
-    def move(self, new_row, new_col) -> bool:
-        """
-        TODO: Check path between new position and old position for other pieces
-        """
-        if (new_row, new_col) not in self.available_moves():
-            print("INVALID MOVE")
-            return False
-
-        destination = self.board[new_row][new_col]
-        if destination is not None and destination.allegiance != self.allegiance:
-            print(f"Captured {destination} at position ({new_row}, {new_col})")
-        elif destination is not None:
-            print(f"Cannot capture {destination}!")
-            return False
-
-        # All conditions passed so move Bishop piece
-        print(f"Moved {self} to position ({new_row}, {new_col})")
-
-        self.board[self.current_row][self.current_col] = None
-        self.board[new_row][new_col] = self
-
-        # Update variables
-        self.moves += 1
-        self.current_row = new_row
-        self.current_col = new_col
-        return True
 
     def available_moves(self):
+        """
+        TODO: Check squares in between positions for pieces
+        Store in such a way that it shows all indices in between?
+        """
         movements = []
 
         # Bishop moves diagonally, so we check all four diagonal directions
@@ -130,56 +134,25 @@ class Bishop(Piece):
 
 
 class Queen(Piece):
-    def __init__(self, allegiance, board, current_pos):
-        points = 4
-        self.moves = 0
-        self.allegiance = allegiance
-        self.current_row = current_pos[0]
-        self.current_col = current_pos[1]
-        self.board = board
-        # self.board[self.current_col][self.current_row] = self
-        if allegiance == 'black':
-            self.texture = arcade.load_texture('pieces_png/black-queen.png')
-        else:
-            self.texture = arcade.load_texture('pieces_png/white-queen.png')
-
-    def move(self, new_row, new_col) -> bool:
-        if (new_row, new_col) not in self.available_moves():
-            print("INVALID MOVE!")
-            return False
-        destination = self.board[new_row][new_col]
-        if destination is not None and destination.allegiance != self.allegiance:
-            print(f"Captured {destination} at position ({new_row}, {new_col})")
-        elif destination is not None:
-            print("Cannot capture that piece!")
-
-        print(f"Moved {self} to position ({new_row}, {new_col})")
-        self.board[self.current_row][self.current_col] = None
-        self.board[new_row][new_col] = self
-
-        # Update variables
-        self.moves += 1
-        self.current_row = new_row
-        self.current_col = new_col
-        return True
-
     def available_moves(self):
         movements = []
 
         # Queen moves diagonally, so we check all four diagonal directions
-        for diagonal_row, diagonal_col in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+        # Queen also moves horizontally and vertically
+        for diagonal_row, diagonal_col in [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (0, -1), (1, 0), (0, 1)]:
             row, col = self.current_row + diagonal_row, self.current_col + diagonal_col
             while 0 <= row < 8 and 0 <= col < 8:
-                movements.append((row, col))
+                if self.board[row][col] is not None:
+                    if self.board[row][col].allegiance == self.allegiance:
+                        break
+                    else:
+                        movements.append((row, col))
+                        break
+                else:
+                    movements.append((row, col))
 
                 row += diagonal_row
                 col += diagonal_col
-
-        # Queen may also move vertically and horizontally
-        # TODO: Includes current position
-        for i in range(0, 8):
-            movements.append((self.current_row, i))
-            movements.append((i, self.current_col))
 
         return movements
 
@@ -188,58 +161,19 @@ class Queen(Piece):
             return '♛'
         return '♕'
 
-        # return f"{self.allegiance} Queen"
 
-
-class King:
-    def __init__(self, allegiance, board, current_pos):
-        points = 4
-        self.moves = 0
-        self.allegiance = allegiance
-        self.board = board
-        self.current_row = current_pos[0]
-        self.current_col = current_pos[1]
-        self.board[self.current_row][self.current_col] = self
-        if allegiance == 'black':
-            self.texture = arcade.load_texture('pieces_png/black-king.png')
-        else:
-            self.texture = arcade.load_texture('pieces_png/white-king.png')
-
-    def move(self, new_row, new_col) -> bool:
-        if (new_row, new_col) not in self.available_moves():
-            print("INVALID MOVE!")
-            return False
-
-        destination = self.board[new_row][new_col]
-        if destination is not None and destination.allegiance != self.allegiance:
-            print(f"Captured {destination} at position ({new_row}, {new_col})")
-        elif destination is not None:
-            print("Cannot capture that piece!")
-
-        print(f"Moved {self} to position ({new_row}, {new_col})")
-        self.board[self.current_row][self.current_col] = None
-        self.board[new_row][new_col] = self
-
-        # Update variables
-        self.moves += 1
-        self.current_row = new_row
-        self.current_col = new_col
-        return True
-
+class King(Piece):
     def available_moves(self):
         movements = []
+        for move_row, move_col in [(-1, -1), (-1, 1), (1, -1), (1, 1), (-1, 0), (0, -1), (1, 0), (0, 1)]:
+            row, col = self.current_row + move_row, self.current_col + move_col
+            if 0 <= row < 8 and 0 <= col < 8:
+                # If king wont go into check add to movements
+                if self.board[row][col] is None or self.board[row][col].allegiance == self.allegiance:
+                    movements.append((row, col))
 
-        movements.append((self.current_row - 1, self.current_col))
-        movements.append((self.current_row - 1, self.current_col + 1))
-        movements.append((self.current_row - 1, self.current_col - 1))
-
-        movements.append((self.current_row, self.current_col))
-        movements.append((self.current_row, self.current_col + 1))
-        movements.append((self.current_row, self.current_col - 1))
-
-        movements.append((self.current_row + 1, self.current_col))
-        movements.append((self.current_row + 1, self.current_col + 1))
-        movements.append((self.current_row + 1, self.current_col - 1))
+                row += move_row
+                col += move_col
 
         return movements
 
@@ -248,21 +182,25 @@ class King:
             return '♚'
         return '♔'
 
-        # return f"{self.allegiance} King"
-
 
 if __name__ == "__main__":
     chess_board = [[None for _ in range(8)] for _ in range(8)]
 
     # bish = Bishop("Black", chess_board, 0, 0)
     #bish = Bishop("White", chess_board, [3, 3])
-    king = King("Black", chess_board, [1, 2])
+    king = King("Black", chess_board, [1, 2], "pieces_png/black-king.png")
+    queen = Queen("White", chess_board, [2, 2], "pieces_png/white-queen.png")
     #king = King("White", chess_board, 2, 2)
 
     for row in chess_board:
         print(row)
 
-    print(king.available_moves())
+    print(queen.available_moves())
+    print(queen.move([1, 2]))
+    move = queen.available_moves()
+    print(move)
+    for r, c in move:
+        print(chess_board[r][c])
 
     for row in chess_board:
         print(row)
