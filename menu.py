1cdd8278bffbff6599f6f0e90308b4d41ea94cde
Mish Wilson <mwilso33@uvm.edu>
2024-03-24 15:31:53 -0400

    lil lad things: made the board even cooler

--- menu.py
+++ menu.py
@@ -33,7 +33,6 @@ LIGHT_SQUARE_COLOR = arcade.color.ALMOND
 DARK_SQUARE_COLOR = arcade.color.SADDLE_BROWN
 
 class Button:
-    #SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size(
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -45,15 +44,13 @@ class Button:
         self.is_hovered = False
         self.is_clicked = False
 
-    def draw(self, text, size):
+    def draw(self):
         if self.is_clicked:
             arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.clicked_color)
         elif self.is_hovered:
             arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.hover_color)
         else:
             arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color)
-        #arcade.draw_text(text, self.x - self.width // 10, self.y - self.height // 10, arcade.color.BLACK, size)
-        arcade.draw_text(text, self.x - len(text) * 5, self.y - self.height // 10, arcade.color.BLACK, 16)
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.is_hovered = (self.x - self.width / 2 < x < self.x + self.width / 2 and
@@ -72,7 +69,6 @@ class MenuView(arcade.View):
         self.logo = arcade.load_texture("pieces_png/chess_logo.png")
         self.play_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 200, 60)
         self.tutorial_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 200, 60)
-        self.game_view = None  # Placeholder for the game view instance
 
     def on_show(self):
         arcade.set_background_color(BG_COLOR)
@@ -100,8 +96,8 @@ class MenuView(arcade.View):
 
         # Draw the logo stuff
         self.logo.draw_sized(center_x=CENTER_WIDTH, center_y=CENTER_HEIGHT, width=600, height=500)
-        self.play_button.draw("Play", 16)
-        self.tutorial_button.draw("Quit", 16)
+        self.play_button.draw()
+        self.tutorial_button.draw()
 
     # def on_key_press(self, key, modifiers):
     #     if key == arcade.key.KEY_1:
@@ -125,10 +121,9 @@ class MenuView(arcade.View):
         self.play_button.on_mouse_press(x, y, button, modifiers)
         self.tutorial_button.on_mouse_release(x, y, button, modifiers)
 
-
     def update(self, delta_time):
         if self.play_button.is_clicked:
-            game_view = GameView()
+            game_view = board.Board()
             self.window.show_view(game_view)
         # if self.tutorial_button.is_clicked:
         #
@@ -139,69 +134,43 @@ class MenuView(arcade.View):
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
-        self.logo = arcade.load_texture("pieces_png/game_mode.png")
-        self.player_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 250, 60)
-        self.computer_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 250, 60)
+        self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
+                                         center_y=SCREEN_HEIGHT // 2)
 
-        # self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
-                                        #s center_y=SCREEN_HEIGHT // 2)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        self.player_button.on_mouse_motion(x, y, dx, dy)
-        self.computer_button.on_mouse_motion(x, y, dx, dy)
-
-    def on_mouse_press(self, x, y, button, modifiers):
-        self.player_button.on_mouse_press(x, y, button, modifiers)
-        self.computer_button.on_mouse_press(x, y, button, modifiers)
-
-    def on_mouse_release(self, x, y, button, modifiers):
-        self.player_button.on_mouse_press(x, y, button, modifiers)
-        self.computer_button.on_mouse_release(x, y, button, modifiers)
-        if self.player_button.is_clicked:
-            self.game_view = GameView()  # Create an instance of the game view
-            self.window.show_view(self.game_view)  # Show the game view
-        elif self.computer_button.is_clicked:
-            if self.game_view:
-                self.window.show_view(self)
-
-    # def on_show(self):
-    #     arcade.set_background_color(arcade.color.WHITE)
-    #     self.chess_piece.position = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
     def on_show(self):
-        arcade.set_background_color(BG_COLOR)
+        arcade.set_background_color(arcade.color.WHITE)
+        self.chess_piece.position = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_text("Chess Game - Player vs Player", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 200,
+                         TEXT_COLOR, font_size=30, anchor_x="center")
+        self.chess_piece.draw()
 
-        # Make even squares
+    def on_update(self, delta_time):
+        self.chess_piece.angle += 1
 
-        ROWS = SCREEN_WIDTH // SQUARE_WIDTH
-        COLS = SCREEN_HEIGHT // SQUARE_HEIGHT
 
-        for row in range(ROWS):
-            for col in range(COLS):
-                x = col * SQUARE_WIDTH
-                y = row * SQUARE_HEIGHT
+# Testing
+class ComputerGameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
+                                         center_y=SCREEN_HEIGHT // 2)
 
-                if (row + col) % 2 == 0:
-                    color = LIGHT_SQUARE_COLOR
-                else:
-                    color = DARK_SQUARE_COLOR
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BLACK)
+        self.chess_piece.position = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
 
-                arcade.draw_rectangle_filled(x + SQUARE_WIDTH // 2, y + SQUARE_HEIGHT // 2, SQUARE_WIDTH, SQUARE_HEIGHT,
-                                             color)
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Chess Game - Player vs Computer", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 200,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+        self.chess_piece.draw()
 
-        # Draw the logo stuff
-        self.logo.draw_sized(center_x=CENTER_WIDTH, center_y=CENTER_HEIGHT + 120, width=600, height=200)
-        self.player_button.draw("Player vs Player", 12)
-        self.computer_button.draw("Player vs Computer", 12)
-    def update(self, delta_time):
-        if self.player_button.is_clicked:
-            game_view = board.Board("player")
-            self.window.show_view(game_view)
-        if self.computer_button.is_clicked:
-            game_view = board.Board("computer")
-            self.window.show_view(game_view)
+    def on_update(self, delta_time):
+        self.chess_piece.angle -= 1
+
 
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
@@ -212,7 +181,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
--- test_main.py
+++ test_main.py
@@ -54,5 +54,4 @@ def main():
     arcade.run()
 
 if __name__ == "__main__":
-    for i in range(4):
-        print (i)
+    main()
