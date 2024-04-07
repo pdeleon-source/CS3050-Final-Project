5af0eeb715ccd9de9d048fcc62d71a5627f57b42
Mish Wilson <mwilso33@uvm.edu>
2024-04-07 01:12:16 -0400

    making menu sutff, hard times :(

--- menu.py
+++ menu.py
@@ -15,8 +15,11 @@ import sys
 import time
 
 # Define screen dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# SCREEN_WIDTH = 800
+# SCREEN_HEIGHT = 600
+
+SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
+
 SCREEN_TITLE = "Chess Game Intro"
 CENTER_WIDTH = SCREEN_WIDTH // 2
 CENTER_HEIGHT = SCREEN_HEIGHT // 2
@@ -24,8 +27,8 @@ CENTER_HEIGHT = SCREEN_HEIGHT // 2
 # Set the dimensions of the chessboard
 ROWS = 8
 COLS = 8
-SQUARE_WIDTH = SCREEN_WIDTH // 8
-SQUARE_HEIGHT = SCREEN_HEIGHT // 8
+SQUARE_WIDTH = 800 // 8
+SQUARE_HEIGHT = 800 // 8
 
 # Define default colors
 BG_COLOR = arcade.color.BRUNSWICK_GREEN
@@ -49,7 +52,7 @@ MENU_SOUND = "sounds/bob.wav"
 
 
 class Button:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, texture):
         self.x = x
         self.y = y
         self.width = width
@@ -61,16 +64,37 @@ class Button:
         self.is_clicked = False
         # Volume of sound
         self.volume = 1.0
+        self.texture = texture
 
+    # TODO: Fix this, too repetitive
     def draw(self, text, color):
         if self.is_clicked:
-            arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.clicked_color)
+            if self.texture is None:
+                arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.clicked_color)
+            else:
+                arcade.draw_texture_rectangle(self.x,
+                                              self.y,
+                                              self.width, self.height,
+                                              self.texture)
         elif self.is_hovered:
-            arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.hover_color)
+            if self.texture is None:
+                arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.hover_color)
+            else:
+                arcade.draw_texture_rectangle(self.x,
+                                              self.y,
+                                              self.width, self.height,
+                                              self.texture)
         else:
-            arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, color)
+            if self.texture is None:
+                arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, color)
+            else:
+                arcade.draw_texture_rectangle(self.x,
+                                              self.y,
+                                              self.width, self.height,
+                                              self.texture)
         # arcade.draw_text(text, self.x - self.width // 10, self.y - self.height // 10, arcade.color.BLACK, size)
-        arcade.draw_text(text, self.x - len(text) * 5, self.y - self.height // 10, arcade.color.BLACK, 16)
+        if self.texture is None:
+            arcade.draw_text(text, self.x - len(text) * 5, self.y - self.height // 10, arcade.color.BLACK, 16)
 
     def on_mouse_motion(self, x, y, dx, dy):
         self.is_hovered = (self.x - self.width / 2 < x < self.x + self.width / 2 and
@@ -101,10 +125,13 @@ class MenuView(arcade.View):
     def __init__(self, theme):
         super().__init__()
         self.logo = arcade.load_texture("pieces_png/chess_logo.png")
-        self.play_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 25, 200, 40)  # Center - 40, Height was 60
-        self.tutorial_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 70, 200, 40)  # Center - 110, Height was 60
-        self.settings_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 115, 200, 40)
-        self.quit_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 160, 200, 40)
+        self.settings_png = arcade.load_texture("pieces_png/settings_cog.png")
+
+        self.play_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 25, 200, 40, None)  # Center - 40, Height was 60
+        self.tutorial_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 70, 200, 40, None)  # Center - 110, Height was 60
+        #self.settings_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 115, 200, 40, None)
+        self.settings_button = Button(SCREEN_WIDTH - SQUARE_WIDTH // 2, SCREEN_HEIGHT - SQUARE_HEIGHT // 2, 60, 60, self.settings_png)
+        self.quit_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 160, 200, 40, None)
         self.game_view = None  # Placeholder for the game view instance
 
         # audio = arcade.load_sound(MENU_SOUND, True)
@@ -122,13 +149,13 @@ class MenuView(arcade.View):
 
         # Make even squares
 
-        ROWS = SCREEN_WIDTH // SQUARE_WIDTH
-        COLS = SCREEN_HEIGHT // SQUARE_HEIGHT
+        r = SCREEN_HEIGHT // SQUARE_WIDTH
+        c = SCREEN_WIDTH // SQUARE_HEIGHT
 
         light_square_color, dark_square_color = self.theme_manager.get_colors()
 
-        for row in range(ROWS):
-            for col in range(COLS):
+        for row in range(r + 1):
+            for col in range(c + 1):
                 x = col * SQUARE_WIDTH
                 y = row * SQUARE_HEIGHT
 
@@ -144,7 +171,8 @@ class MenuView(arcade.View):
         self.logo.draw_sized(center_x=CENTER_WIDTH, center_y=CENTER_HEIGHT, width=700, height=580)  # Was w=600h=500
         self.play_button.draw("Play", BUTTON_COLOR)
         self.tutorial_button.draw("Tutorials", BUTTON_COLOR)
-        self.settings_button.draw("Settings", BUTTON_COLOR)
+        # self.settings_button.draw("Settings", BUTTON_COLOR)
+        self.settings_button.draw("", BUTTON_COLOR)
         self.quit_button.draw("Quit", BUTTON_COLOR)
 
     # def on_key_press(self, key, modifiers):
@@ -194,9 +222,9 @@ class GameView(arcade.View):
     def __init__(self, theme):
         super().__init__()
         self.logo = arcade.load_texture("pieces_png/game_mode.png")
-        self.player_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 250, 60)
-        self.computer_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 250, 60)
-        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 180, 250, 60)
+        self.player_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 250, 60, None)
+        self.computer_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 250, 60, None)
+        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 180, 250, 60, None)
         # self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
         # s center_y=SCREEN_HEIGHT // 2)
 
@@ -276,9 +304,9 @@ class SettingsView(arcade.View):
     def __init__(self, theme):
         super().__init__()
         self.logo = arcade.load_texture("pieces_png/settings_logo.png")
-        self.sound_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 250, 60)
-        self.theme_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 250, 60)
-        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 180, 250, 60)
+        self.sound_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 40, 250, 60, None)
+        self.theme_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 110, 250, 60, None)
+        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 180, 250, 60, None)
 
         # self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
         # s center_y=SCREEN_HEIGHT // 2)
@@ -365,12 +393,12 @@ class ThemeView(arcade.View):
         self.bg_color = BG_COLOR"""
 
         # Theme buttons
-        self.default_button = Button(CENTER_WIDTH - 200, CENTER_HEIGHT + 150, 200, 200)
-        self.pink_button = Button(CENTER_WIDTH + 200, CENTER_HEIGHT + 150, 200, 200)
-        self.ocean_button = Button(CENTER_WIDTH - 200, CENTER_HEIGHT - 100, 200, 200)
-        self.midnight_button = Button(CENTER_WIDTH + 200, CENTER_HEIGHT - 100, 200, 200)
+        self.default_button = Button(CENTER_WIDTH - 200, CENTER_HEIGHT + 150, 200, 200, None)
+        self.pink_button = Button(CENTER_WIDTH + 200, CENTER_HEIGHT + 150, 200, 200, None)
+        self.ocean_button = Button(CENTER_WIDTH - 200, CENTER_HEIGHT - 100, 200, 200, None)
+        self.midnight_button = Button(CENTER_WIDTH + 200, CENTER_HEIGHT - 100, 200, 200, None)
 
-        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 250, 250, 60)
+        self.return_button = Button(CENTER_WIDTH, CENTER_HEIGHT - 250, 250, 60, None)
 
         # Initialize theme management object
         self.theme_manager = ManageTheme(theme)
