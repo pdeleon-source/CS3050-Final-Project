565136dcce18792e76d321ea1bb8a110a5b9c151
hannah <hgsherid@uvm.edu>
2024-04-14 20:39:43 -0400

    Eeek sorry

--- menu.py
+++ menu.py
@@ -6,47 +6,29 @@ visit a settings menu to change the theme or toggle sound.
 
 import arcade
 import board
-import sys
 import time
 import tutorial as t
 import setting as s
 import arcade.gui
 from theme_manager import ManageTheme
-from game_manager import ManageGame
 from sound_manager import ManageSound
-
+import game as g
 import arcade.gui.widgets
 import arcade.gui.widgets
 from arcade.experimental.uistyle import UIFlatButtonStyle
 
+
+# Find screen measurements
 SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
 
-SCREEN_TITLE = "Chess Game Intro"
 CENTER_WIDTH = SCREEN_WIDTH // 2
 CENTER_HEIGHT = SCREEN_HEIGHT // 2
 
 # Set the dimensions of the chessboard
 ROWS = 8
 COLS = 8
-SQUARE_WIDTH = 800 // 8
-SQUARE_HEIGHT = 800 // 8
-
-# Define default colors
-BG_COLOR = arcade.color.BRUNSWICK_GREEN
-
-TEXT_COLOR = arcade.color.BLACK
-LIGHT_SQUARE_COLOR = arcade.color.ALMOND
-DARK_SQUARE_COLOR = arcade.color.SADDLE_BROWN
-
-# Define sound effects - must be .wav :(
-BUTTON_SOUND = "sounds/doink.wav"
-MENU_SOUND = "sounds/bob.wav"
-
-theme_manager = ManageTheme("default")
-game_manager = ManageGame("_")
-sound_manager = ManageSound(1)
-
-VOLUME = 1
+SQUARE_WIDTH = 800 // ROWS
+SQUARE_HEIGHT = 800 // COLS
 
 default_style = {
     "normal": UIFlatButtonStyle(
@@ -84,11 +66,6 @@ default_style = {
 }
 
 
-def play_button_sound():
-    audio = arcade.load_sound(BUTTON_SOUND, False)
-    arcade.play_sound(audio, sound_manager.get_volume(), -1, False)
-
-
 class MenuView(arcade.View):
     """
         The main menu. Has options which allow the user to choose a game mode,
@@ -98,10 +75,17 @@ class MenuView(arcade.View):
         super().__init__()
         self.manager = arcade.gui.UIManager()
 
+        # Define Managers
+        global theme_manager, game_manager, sound_manager
+
+        # Open Managers
+        theme_manager = ManageTheme("default")
+        sound_manager = ManageSound(1)
+
         # Load game logo
-        self.logo = arcade.load_texture("pieces_png/chess_logo.png")
-        self.settings_png = arcade.load_texture("pieces_png/settings_cog.png")
-        self.tutorial_png = arcade.load_texture("pieces_png/Black_question_mark.png")
+        self.logo = arcade.load_texture("banners/chess_logo.png")
+        self.settings_png = arcade.load_texture("assets/settings_cog.png")
+        self.tutorial_png = arcade.load_texture("assets/Black_question_mark.png")
 
         # Create button objects for each option
         play_button = arcade.gui.UIFlatButton(x=CENTER_WIDTH - 100,
@@ -134,14 +118,14 @@ class MenuView(arcade.View):
         @play_button.event("on_click")
         def on_click_switch_button(event):
             sound_manager.play_button_sound()
-            game_view = GameView(theme_manager)
+            game_view = g.GameView(theme_manager)
             self.window.show_view(game_view)
 
         @quit_button.event("on_click")
         def on_click_switch_button(event):
             sound_manager.play_button_sound()
             time.sleep(.15)
-            sys.exit()
+            arcade.exit()
 
         @settings_button.event("on_click")
         def on_click_switch_button(event):
@@ -176,9 +160,6 @@ class MenuView(arcade.View):
 
         self.theme_manager = ManageTheme(theme)
         self.volume = volume
-
-    def on_show(self):
-        arcade.set_background_color(BG_COLOR)
 
     def on_draw(self):
         """
@@ -220,170 +201,3 @@ class MenuView(arcade.View):
     def on_hide_view(self):
         # Disable the UIManager when the view is hidden.
         self.manager.disable()
-
-class GameView(arcade.View):
-    def __init__(self, theme):
-        super().__init__()
-
-        self.manager = arcade.gui.UIManager()
-        self.logo = arcade.load_texture("pieces_png/game_banner.png")
-        self.settings_png = arcade.load_texture("pieces_png/settings_cog.png")
-        self.tutorial_png = arcade.load_texture("pieces_png/Black_question_mark.png")
-
-        player_button = arcade.gui.UIFlatButton(x=CENTER_WIDTH - 125,
-                                                y=CENTER_HEIGHT,
-                                                width=250,
-                                                height=60,
-                                                text="Player vs Player",
-                                                style=default_style)
-
-        computer_button = arcade.gui.UIFlatButton(x=CENTER_WIDTH - 125,
-                                                  y=CENTER_HEIGHT - 70,
-                                                  width=250,
-                                                  height=60,
-                                                  text="Player vs Computer",
-                                                  style=default_style)
-
-        return_button = arcade.gui.UIFlatButton(x=CENTER_WIDTH - 125,
-                                                y=CENTER_HEIGHT - 140,
-                                                width=250,
-                                                height=60,
-                                                text="Return to Main Menu",
-                                                style=default_style)
-
-        settings_button = arcade.gui.UITextureButton(x=SCREEN_WIDTH - (SQUARE_WIDTH // 2) - 30,
-                                                     y=(SCREEN_HEIGHT - SQUARE_HEIGHT // 2) - 60,
-                                                     width=60,
-                                                     height=60,
-                                                     texture=self.settings_png)
-
-        tutorial_button = arcade.gui.UITextureButton(x=(SCREEN_WIDTH - (SQUARE_WIDTH // 2) * 3) - 30,
-                                                     y=(SCREEN_HEIGHT - SQUARE_HEIGHT // 2) - 60,
-                                                     width=60,
-                                                     height=60,
-                                                     texture=self.tutorial_png)
-
-        @player_button.event("on_click")
-        def on_click_switch_button(event):
-            sound_manager.play_button_sound()
-            game_view = board.Board("player")
-            self.window.show_view(game_view)
-
-        @computer_button.event("on_click")
-        def on_click_switch_button(event):
-            sound_manager.play_button_sound()
-            game_view = board.Board("computer")
-            self.window.show_view(game_view)
-
-        @return_button.event("on_click")
-        def on_click_switch_button(event):
-            sound_manager.play_button_sound()
-            game_view = MenuView(theme_manager.theme, VOLUME)
-            self.window.show_view(game_view)
-
-        @settings_button.event("on_click")
-        def on_click_switch_button(event):
-            sound_manager.play_button_sound()
-            settings_menu = s.SettingsMenu(
-                "Settings",
-                "Volume",
-                theme_manager,
-                sound_manager
-            )
-            self.manager.add(
-                settings_menu
-            )
-
-        @tutorial_button.event("on_click")
-        def on_click_switch_button(event):
-            sound_manager.play_button_sound()
-            tutorial_menu = t.SubMenu(
-                "Tutorial Menu"
-            )
-            self.manager.add(
-                tutorial_menu
-            )
-
-        self.manager.add(tutorial_button)
-        self.manager.add(settings_button)
-
-        self.manager.add(player_button)
-        self.manager.add(computer_button)
-        self.manager.add(return_button)
-
-        # self.chess_piece = arcade.Sprite("pieces_png/white-pawn.png", center_x=SCREEN_WIDTH // 2,
-        # s center_y=SCREEN_HEIGHT // 2)
-
-        # Create theme manager object and set theme
-        theme_manager = ManageTheme(theme)
-
-    def on_show_view(self):
-        """ This is run once when we switch to this view """
-        arcade.set_background_color(BG_COLOR)
-
-        # Enable the UIManager when the view is showm.
-        self.manager.enable()
-
-    def on_draw(self):
-        self.clear()
-        arcade.start_render()
-
-        # Make even squares
-
-        r = SCREEN_HEIGHT // SQUARE_WIDTH
-        c = SCREEN_WIDTH // SQUARE_HEIGHT
-
-        theme = theme_manager.theme
-        bg_color, black_capture_bg, white_capture_bg = theme_manager.get_background(theme)
-        light_square_color, dark_square_color = theme_manager.get_theme(theme)
-
-        for row in range(r + 1):
-            for col in range(c + 1):
-                x = col * SQUARE_WIDTH
-                y = row * SQUARE_HEIGHT
-
-                if (row + col) % 2 == 0:
-                    color = light_square_color
-                else:
-                    color = dark_square_color
-
-                arcade.draw_rectangle_filled(x + SQUARE_WIDTH // 2, y + SQUARE_HEIGHT // 2, SQUARE_WIDTH, SQUARE_HEIGHT,
-                                             color)
-
-        arcade.draw_rectangle_outline(center_x= CENTER_WIDTH,
-                                      center_y=CENTER_HEIGHT - 40,
-                                      width=295,
-                                      height=260,
-                                      color=arcade.color.WHITE,
-                                      border_width=4)
-
-        arcade.draw_rectangle_outline(center_x=CENTER_WIDTH,
-                                      center_y=CENTER_HEIGHT - 40,
-                                      width=275,
-                                      height=240,
-                                      color=bg_color,
-                                      border_width=3)
-
-        arcade.draw_rectangle_filled(center_x=CENTER_WIDTH,
-                                     center_y=CENTER_HEIGHT - 40,
-                                     width=270,
-                                     height=230,
-                                     color=black_capture_bg)
-
-        self.logo.draw_sized(center_x=CENTER_WIDTH, center_y=CENTER_HEIGHT + 100, width=700, height=580)
-        self.manager.draw()
-
-    def on_hide_view(self):
-        # Disable the UIManager when the view is hidden.
-        self.manager.disable()
-
-
-def main():
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    menu_view = MenuView("default", 1.0)
-    window.show_view(menu_view)
-    arcade.run()
-
-
-if __name__ == "__main__":
-    main()
